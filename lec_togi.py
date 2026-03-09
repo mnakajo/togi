62d36d9f638e1ba6eab97f407a107984a29d16be
mnakajo <116793839+mnakajo@users.noreply.github.com>
2026-03-09 17:04:17 +0900

    Update lec_togi.py

--- lec_togi.py
+++ lec_togi.py
@@ -3,24 +3,73 @@ import streamlit as st
 import pydeck as pdk
 import plotly.express as px
 
-st.title('都議会議員調査ダッシュボード')
-st.header('都議会議員調査：2018-2021')
+st.title('都議会議員調査時系列')
+st.header('2018-2025')
 st.subheader('津田塾大学中條研究室')
+st.markdown("<hr>",unsafe_allow_html=True)
 
-df_ts =pd.read_csv('./csv_data/tsdata.csv')
+from pathlib import Path
+import pandas as pd
 
-show_df = st.checkbox('Show DataFrame')
-if show_df == True:
-    st.write(df_ts)
+BASE_DIR = Path(__file__).parent
+df_ts =pd.read_csv(BASE_DIR/"csv_data"/"tsdata8.csv")
 
+#df_ts['id'] = range(len(df_ts))
 fig=px.scatter(df_ts,
                 x='イデオロギー',
                 y='知事評価',
-                range_x=[0,11],
-                range_y=[0,11],
-                color='会派',
+                range_x=[-1,11],
+                range_y=[-1,11],
+                #color='会派',
+                #animation_group='id',
+                animation_frame='調査年')
+#st.plotly_chart(fig)
+
+df_ideo_counts = (
+    df_ts.groupby(['調査年','イデオロギー'])
+         .size()
+         .reset_index(name='頻度')
+)
+
+fig2=px.bar(df_ideo_counts,
+                x='イデオロギー',
+                y="頻度",
+                range_y=[0, df_ideo_counts['頻度'].max()+5] ,
+                range_x=[-1,11],
+                color='イデオロギー', 
+                color_continuous_scale='RdBu' ,
                 animation_frame='調査年')
 
-st.plotly_chart(fig)
+fig2.update_layout(coloraxis_showscale=False)
 
-st.text('出典：津田塾大学　東京都議会議員調査2018-2021（津田塾大学中條研究室）')
+st.markdown("#### イデオロギー")
+st.write("異なる政治立場を表すとき、「保守」と「リベラル」、あるいは「右派」と「左派」などと表現することがあります。最も右派・保守的な立場を10、最も左派・リベラルな立場を0とした場合、あなたの政治的立場に最も近い数字をお答えください。")
+st.plotly_chart(fig2, use_container_width=True)
+st.caption('出典：津田塾大学　東京都議会議員調査2018-2025（津田塾大学中條研究室）')
+
+df_gov_counts = (
+    df_ts.groupby(['調査年','知事評価'])
+         .size()
+         .reset_index(name='頻度')
+)
+
+fig3=px.bar(df_gov_counts,
+                x='知事評価',
+                y="頻度",
+                range_y=[0, df_gov_counts['頻度'].max()+5] ,
+                range_x=[-1,11],
+                color='知事評価', 
+                color_continuous_scale="Greens",
+                animation_frame='調査年')
+
+fig3.update_layout(coloraxis_showscale=False)
+
+st.markdown("#### 知事評価")
+st.write("現在の東京都知事の仕事ぶりについて、0（全くやっていない）から10（とてもよくやっている）とすると、あなたの評価は何点でしょうか。")
+st.plotly_chart(fig3, use_container_width=True)
+st.caption('出典：津田塾大学　東京都議会議員調査2018-2025（津田塾大学中條研究室）')
+
+st.markdown(
+    "<hr><p style='text-align:center; color:gray; font-size:12px;'>© 2026 Miwa Nakajo. All rights reserved.</p>",
+    unsafe_allow_html=True
+)
